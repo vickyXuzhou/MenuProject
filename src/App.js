49841051a7a7c076a9465c9564eb191f3455b921
Nchang victory <97144931+vickyXuzhou@users.noreply.github.com>
2023-03-29 23:39:22 +0800

    updating buttons

--- src/App.js
+++ src/App.js
@@ -7,9 +7,25 @@ function App() {
   const [menuItem, setMenuItem] = useState(items)
 
   function filterItems(item){
+    if (item==="all"){
+      return(setMenuItem(items))
+      ;
+    }
+    
     let filteredEl = items.filter(elem=>elem.category===item)
-   return setMenuItem(filteredEl)
+   return(setMenuItem(filteredEl))
   }
+  
+  
+  // const filterItems = (category) => {
+  //   if (category === 'all') {
+  //     setMenuItems(items);
+  //     return;
+  //   }
+  //   const newItems = items.filter((item) => item.category === category);
+  //   setMenuItems(newItems);
+  // };
+
   return <main>
     <section className="menu section">
       <div className='title'>
--- src/Categories.js
+++ src/Categories.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Categories = (props) => {
     
-  return <button onClick={()=>props.filterItems('breakfast')}>breakfast</button>;
+  return <div >
+    <button className="filter-btn" onClick={()=>props.filterItems('all')}>All</button>
+   <button className="filter-btn" onClick={()=>props.filterItems('breakfast')}>breakfast</button>
+
+  </div>;
 };
 
 export default Categories;
